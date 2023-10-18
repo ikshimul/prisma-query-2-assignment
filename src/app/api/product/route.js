c507eb10dd4e3b8a57903c79d8e23bf15855e364
ikshimul <ikshimuluits@gmail.com>
2023-10-18 14:03:57 +0600

    prisma query second assignment

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -21,7 +21,23 @@ export async function POST(req, res) {
   };
   try {
     const prisma = new PrismaClient();
-    let result = await prisma.products.create({
+    // let result = await prisma.products.create({
+    //   data: {
+    //     users: {
+    //       connect: {
+    //         id: 1,
+    //       },
+    //     },
+    //     firstName: "Macbook Air M2",
+    //     metaTitle: "Test meta",
+    //     slug: "mackbook-air",
+    //     summary: "mackbook-air",
+    //     price: 1100,
+    //     discount_price: 1050,
+    //   },
+    // });
+
+    const createProduct = prisma.products.create({
       data: {
         users: {
           connect: {
@@ -36,6 +52,38 @@ export async function POST(req, res) {
         discount_price: 1050,
       },
     });
+
+    const createProductMeta = prisma.product_meta.create({
+      data: {
+        products: {
+          connect: {
+            id: 1,
+          },
+        },
+        key: "color",
+        content: "MatBlack",
+      },
+    });
+
+    const createProductReview = prisma.product_review.create({
+      data: {
+        products: {
+          connect: {
+            id: 1,
+          },
+        },
+        title: "Nice",
+        rating: "5",
+        content: "test",
+      },
+    });
+
+    const result = await prisma.$transaction([
+      createProduct,
+      createProductMeta,
+      createProductReview,
+    ]);
+
     return NextResponse.json({ status: "success", data: result });
   } catch (error) {
     return NextResponse.json({ status: "fail", data: error.toString() });
